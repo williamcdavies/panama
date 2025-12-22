9cdf2e62b9e8df144d658235b521b291001a3e5a
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-22 08:55:11 -0800

    Add docstring to main. Link sys entry to Playwright entry

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,20 @@
+from panama.src.session import entry
+
 import sys
 
 
 def main() -> int:
+    """
+    Docstring for main
+    
+    :return: exit code
+    :rtype: int
+    """
+
+    
+    entry.run()
     return 0
     
-    
+
 if __name__ == '__main__':
     sys.exit(main())
